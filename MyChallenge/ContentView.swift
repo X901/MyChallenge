ee5096a5010bca221512c974cb70b32754c79a97
Basel Baragabah <im.x901@gmail.com>
2024-09-05 18:43:26 +0300

    Initial Commit

---
+++ MyChallenge/ContentView.swift
@@ -0,0 +1,25 @@
+////
+//ContentView.swift
+//MyChallenge
+//
+//Created by Basel Baragabah on 05/09/2024.
+//Copyright © 2024 Basel Baragabah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyChallenge/MyChallengeApp.swift
@@ -0,0 +1,18 @@
+////
+//MyChallengeApp.swift
+//MyChallenge
+//
+//Created by Basel Baragabah on 05/09/2024.
+//Copyright © 2024 Basel Baragabah. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MyChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
